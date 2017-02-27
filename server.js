98b35f8dd8e419fe4190cc218aaf1c68f3608b2b
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-02-27 22:00:28 +0000

    worked on layout ideas for games/characters, going to plan/visualize better and come back

--- server.js
+++ server.js
@@ -22,6 +22,39 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
+//games route
+app.get('/games', function(req, res){
+	res.render('games');
+});
+
+
+//characters route
+app.get('/characters', function(req, res){
+	res.render('characters');
+});
+
+
+//maps route
+app.get('/maps', function(req, res){
+	res.render('maps');
+});
+
+
+
+app.use(function(req, res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+
+
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' +
     app.get('port') + '; press Ctrl-C to terminate');
