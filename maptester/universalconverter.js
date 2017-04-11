276660e022639c5ea938f65958895ca3e73f146a
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-04-11 22:40:56 +0000

    trying something else

---
+++ maptester/universalconverter.js
@@ -0,0 +1,12 @@
+var im = require('imagemagick');
+
+for(var i=0; i < 10; i++){
+	console.log(i);
+	im.convert(['map_ww.jpg', '-resize', '25x120', 'test.jpg'], 
+		function(err, stdout){
+			if (err) throw err;
+			console.log('stdout:', stdout);
+			console.log(i);
+		});
+	console.log(i);
+}
