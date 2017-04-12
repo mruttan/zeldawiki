8f4c265c08db3419175e1ee2729a27bba3b0302e
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-04-12 21:39:01 +0000

    Trying to create a new directory when saving

--- maptester/identifytest.js
+++ maptester/identifytest.js
@@ -3,7 +3,8 @@ var im = require('imagemagick');
 var imageSize = im.identify('map_ww.jpg', function(err, features){
   if (err) throw err;
   console.log(features);
+  console.log(features.width);
   // { format: 'JPEG', width: 3904, height: 2622, depth: 8 } 
 });
 
-console.log(imageSize.features);
+console.log(imageSize.features); 
--- maptester/universalconverter.js
+++ maptester/universalconverter.js
@@ -1,12 +1,27 @@
 var im = require('imagemagick');
 
-for(var i=0; i < 10; i++){
-	console.log(i);
-	im.convert(['map_ww.jpg', '-resize', '25x120', `test_${i}.jpg`], 
-		function(err, stdout){
-			if (err) throw err;
-			console.log('stdout:', stdout);
-			console.log(i);
-		});
-	console.log(i);
-}
+
+var imageSize = im.identify('map_ww.jpg', function(err, features){
+	if (err) throw err;
+  //console.log(features); returns block of info
+  console.log(features.width); //returns width
+
+  var tileSize = 10;
+  console.log(tileSize); //returns 10
+
+
+  //working as intended, but unable to create a new directory to save into, must have existing "tiles" directory made
+  for(var i=0; i < features.width; i+= 100){
+  	im.convert(['map_ww.jpg', '-resize', '25x120', `tiles/test_${i}.jpg`], 
+  		function(err, stdout){
+  			if (err) throw err;
+  			console.log('stdout:', stdout);
+  		});
+  	console.log(i);
+  }
+
+
+
+});
+
+console.log(imageSize.features); //returns nothing
